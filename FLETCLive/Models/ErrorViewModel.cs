074740ceaa10c7e882ecf57b9e102b179cdd39e9
James Flaherty <james.flaherty@fletc.dhs.gov>
2020-08-06 07:28:29 -0400

    Add project files.

---
+++ FLETCLive/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FLETCLive.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
